8453428fe8a4d112a6f6ae4b96ccfdd8cdcabdc4
Ivalene Lima <ivalenelima@gmail.com>
2018-06-28 19:25:16 -0300

    teste

---
+++ maximo.py
@@ -0,0 +1,18 @@
+#tentativa de função
+
+def maximo(x,y):
+
+    if (x > y):
+        return x
+    else:
+        return y
+
+
+def main():
+
+    x = int(input("Digite um número: "))
+    y = int(input("Digite um número: "))
+    print(maximo (x,y))
+
+#----------------
+main()
