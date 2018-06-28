75fb630c32a9e4779f38f623ddaf039fcd4135c5
Ivalene Lima <ivalenelima@gmail.com>
2018-06-28 10:28:49 -0300

    Agora crescente pergunta o número de séries que quer avaliar

--- crescente.py
+++ crescente.py
@@ -1,14 +1,17 @@
 #Exercício 5
 
 def main():
-    n1 = int(input("Digite um número inteiro: "))
-    n2 = int(input("Digite um número inteiro: "))
-    n3 = int(input("Digite um número inteiro: "))
+    nSeries = int(input("Digite o número de séries que vai perguntar: "))
+    
+    for i in range(nSeries):
+        n1 = int(input("Digite um número inteiro: "))
+        n2 = int(input("Digite um número inteiro: "))
+        n3 = int(input("Digite um número inteiro: "))
 
-    if(n3 > n2 > n1):
-        print("crescente")
-    else:
-        print("não está em ordem crescente")
+        if(n3 > n2 > n1):
+            print("crescente")
+        else:
+            print("não está em ordem crescente")
 
 #-----
 main()
